25b7953f91f81be346e7f24ec2cc517e9d796735
mromv <mxromv@gmail.com>
2024-05-27 19:14:01 +0300

    add peft to main file

--- src/fine_tuning.py
+++ src/fine_tuning.py
@@ -11,10 +11,9 @@ import torch
 
 from load_data import load_dataset
 
-from custom_trainer import (
-    CustomTrainer,
-    make_classification_report
-)
+from utils.custom_trainer import CustomTrainer
+from utils.metrics import make_classification_report
+from utils.utils import freeze_model
 
 from datasets import (
     load_metric,
@@ -30,6 +29,11 @@ from transformers import (
     EarlyStoppingCallback
 )
 
+from peft import (
+    LoraConfig,
+    get_peft_model
+)
+
 log = logging.getLogger(__name__)
 
 
@@ -40,7 +44,7 @@ def make_training_pipeline(
         train_conf: dict[str, Any],
         loss: torch.nn.Module,
         checkpoints_dir: str,
-        eval_metric: str="f1"
+        eval_metric: str = "f1"
 ) -> Trainer:
 
     metric = load_metric(eval_metric)
@@ -65,24 +69,10 @@ def make_training_pipeline(
     return trainer
 
 
-def freeze_model(model: AutoModelForSequenceClassification, part: float) -> AutoModelForSequenceClassification:
-    weights = sum([p.numel() for p in model.parameters()])
-    threshold = part * weights
-
-    params = 0
-    for w in model.parameters():
-        if params <= threshold:
-            params += w.numel()
-            w.requires_grad = False
-        else:
-            break
-
-    return model
-
 @hydra.main(version_base=None, config_path="./conf", config_name="config")
 def main(cfg: DictConfig):
     device = torch.device(cfg["general"]["device"]) if torch.cuda.is_available() else torch.device("cpu")
-    data_path = os.path.join(cfg["general"]["data_dir"], "2_preprocessed_data.csv")
+    data_path = os.path.join(cfg["general"]["data_dir"], "preprocessed_data_w_tags.csv")
     output_dir = os.path.join(cfg["general"]["output_dir"], cfg["encoder"]["short_name"])
     checkpoints_dir = os.path.join(cfg["general"]["checkpoints"], cfg["encoder"]["short_name"])
 
@@ -91,6 +81,7 @@ def main(cfg: DictConfig):
     dataset, id2label, label2id = load_dataset(
         data_path=data_path,
         use_col=cfg["data"]["column_use"],
+        target="Category",
         tokenizer=tokenizer,
         test_size=cfg["general"]["test_size"],
         max_length=cfg["data"]["max_length"],
@@ -101,7 +92,22 @@ def main(cfg: DictConfig):
     model = instantiate(
         cfg["encoder"]["model"], num_labels=len(id2label), id2label=id2label, label2id=label2id
     )
-    model = freeze_model(model, cfg["fine_tuning"]["freeze"])
+
+    if cfg["fine_tuning"]["lora"]:
+        output_dir = os.path.join(output_dir, "lora")
+        checkpoints_dir = os.path.join(checkpoints_dir, "lora")
+
+        config = LoraConfig(
+            r=cfg["peft"]["r"],
+            lora_alpha=cfg["peft"]["lora_alpha"],
+            target_modules=cfg["peft"]["target_modules"],
+            lora_dropout=cfg["peft"]["lora_dropout"],
+            bias=cfg["peft"]["bias"],
+            modules_to_save=cfg["peft"]["modules_to_save"]
+        )
+        model = get_peft_model(model, config)
+    else:
+        model = freeze_model(model, cfg["fine_tuning"]["freeze"])
 
     trainer = make_training_pipeline(model, tokenizer, dataset, cfg["training"], cfg["loss"], checkpoints_dir)
     trainer.train()
@@ -110,8 +116,10 @@ def main(cfg: DictConfig):
     report = make_classification_report(trainer, dataset["test"])
 
     # logging
-    log.info(f"Fine tuning result: {cfg['encoder']['short_name']} with loss {cfg['loss']['_target_'].split('.')[-1]}")
-    [log.info(f"{i + 1} epoch: {epoch}") for i, epoch in enumerate(trainer.state.log_history[:-1])]
+    log.info(f"Fine tuning result: "
+             f"{cfg['encoder']['short_name']} with loss: {cfg['loss']['_target_'].split('.')[-1]} "
+             f"lora: {cfg['fine_tuning']['lora']}")
+    [log.info(f"{i + 1}: {epoch}") for i, epoch in enumerate(trainer.state.log_history[:-1])]
     log.info(f"{trainer.state.log_history[-1]}")
     log.info(f"\n{report}")
 
